7a93956618867ad54201065e73420eade8f6569c
Gmzest <107549476+Gmzest@users.noreply.github.com>
2022-11-03 00:28:01 +0300

    Update task_5.py

--- Лабораторная работа 5/task_5.py
+++ Лабораторная работа 5/task_5.py
@@ -3,8 +3,8 @@ import string
 
 def get_random_password(n = 8) -> str:
     ...  # TODO написать функцию генерации случайных паролей
-    return random.sample([i for i in string.digits] +
-                         [i for i in string.ascii_uppercase] +
-                         [i for i in string.ascii_lowercase], k = n)
+    return random.sample([f"{i}" for i in string.digits] +
+                         [f"{i}" for i in string.ascii_uppercase] +
+                         [f"{i}" for i in string.ascii_lowercase], k = n)
 
-print(get_random_password())
+print("".join(get_random_password()))
